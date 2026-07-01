b5a19ab76323c777737c3a416870ddd3d5372227
krishna <krishakula22@gmail.com>
2026-07-01 23:01:00 +0530

    Day3 Maximum subarray

--- Day 3/Maximum_Subarray_Sum.py
+++ Day 3/Maximum_Subarray_Sum.py
@@ -1,18 +1,47 @@
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+# Example 1:
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+# Example 2:
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+
+# Example 3:
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+
 from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
       
-        # Bruteforce approach
-        n = len(nums)
-        max_sum = nums[0]  
+        # max_sum = nums[0]  
+#Brute Force Approach to get the sum of each sum array and find the maximum of it
+        # for i in range(n):
+        #     total = 0
+        #     for j in range(i, n):
+        #         total += nums[j]
+        #         max_sum = max(max_sum, total)
 
-        for i in range(n):
-            for j in range(i, n):
-                total = 0
-                for k in range(i, j+1):
-                    total = total + nums[k]
-                    max_sum = max(total, max_sum)
+# Kadane's Algorithm
+# An iterative dynamic programming technique used to 
+# find the maximum sum of a contiguous subarray within a 1D array of numbers
+
+# The algorithm works by scanning the array while keeping track of two values:
+# Current Subarray Sum: The maximum sum of a subarray that ends at the current index. 
+# Maximum Sum So Far: The overall maximum sum encountered during the entire traversal.
+
+        max_sum = float('-inf')
+        current_sum = 0
+        for num in nums:
+            current_sum = max(0, current_sum + num)
+            max_sum = max(current_sum, max_sum)
 
         return max_sum       
 
